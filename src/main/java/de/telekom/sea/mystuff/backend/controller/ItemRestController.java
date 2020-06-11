5cb9ff45809de50f20a9c36054f8b689b099050d
akless <andre.kless@web.de>
2020-06-11 21:35:38 +0200

    added CORS

--- src/main/java/de/telekom/sea/mystuff/backend/controller/ItemRestController.java
+++ src/main/java/de/telekom/sea/mystuff/backend/controller/ItemRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import de.telekom.sea.mystuff.backend.model.Item;
 import de.telekom.sea.mystuff.backend.repository.ItemRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/")
 public class ItemRestController {
 
